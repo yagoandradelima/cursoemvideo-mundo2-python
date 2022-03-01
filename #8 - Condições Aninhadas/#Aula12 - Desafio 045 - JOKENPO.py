874223f2d9cfb8522ab82b2e6d5977b526ace067
Yago Andrade <yagoandradelima@outlook.com>
2022-03-01 16:33:52 -0300

    update de novas atividades

---
+++ #8 - Condições Aninhadas/#Aula12 - Desafio 045 - JOKENPO.py
@@ -0,0 +1,39 @@
+#Aula12 - Desafio 045 - JOKENPO
+
+from random import choice
+from time import sleep
+
+lista = ['Pedra','Papel','Tesoura']
+
+print('*'*45)
+print(' '*20, 'JOKENPO')
+print('*'*45)
+print('\033[7mVamos jogar JOKENPO. Você consegue me vencer?\033[m')
+user = str(input('Pedra, Papel ou Tesoura? ')).strip().upper()
+print('*'*45)
+print()
+print('\033[1;37;40mJO-\033[m')
+sleep(1)
+print('\033[1;37;40mKEN-\033[m')
+sleep(1)
+print('\033[1;37;40mPO!\033[m')
+sleep(1)
+print('Processando...')
+sleep(2)
+pc = choice(lista)
+print(f'Eu escolhi {pc}')
+
+if (user.upper() == 'TESOURA' and pc.upper() == 'PAPEL') or (user.upper() == 'PAPEL' and pc.upper() == 'PEDRA') or (user.upper() == 'PEDRA' and pc.upper() == 'TESOURA'):
+    print()
+    print('Você me venceu!')
+
+elif user.upper() == pc.upper():
+    print('Empatamos!')
+
+else: 
+    print('Eu venci!')
+
+print('Fim do programa')
+
+
+
