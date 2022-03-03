7919d49dfb3ff8b5013baa653c10f8aff2d18ec5
Yago Andrade <yagoandradelima@outlook.com>
2022-03-03 18:02:16 -0300

    update novas atividades

---
+++ #11 - While - Break/#Aula15 - Desafio 066 - CONTADOR.py
@@ -0,0 +1,11 @@
+soma = c = num = 0
+
+while True:
+    num = int(input('Digite um número: '))
+
+    if num == 999:
+        break
+    soma += num
+    c += 1
+
+print(f'Foram digitados {c} números e a soma deles é igual a {soma}')
---
+++ #11 - While - Break/#Aula15 - Desafio 068 - PAR OU IMPAR.py
@@ -0,0 +1,28 @@
+#Aula15 - Desafio 068
+
+from random import randint
+c = 0
+print('-='*10)
+print('\033[7mJOGO DO PAR OU ÍMPAR\033[m')
+print('-='*10)
+while True:
+    nUser = int(input('Diga um valor: '))
+    choice = str(input('Par ou Impar [P/I]?')).upper().strip()[0]
+    nPC = randint(0, 10)
+    soma = nUser + nPC
+    if soma % 2 == 0 and choice == 'P':
+        print()
+        print(f'Acertou! Eu escolhi o número {nPC} e a soma deu {soma}')
+        print('Vamos de novo!')
+        print('-'*20)    
+    elif soma % 2 != 0 and choice == 'I':
+        print()
+        print(f'Acertou! Eu escolhi o número {nPC} e a soma deu {soma}')
+        print('Vamos de novo!')
+        print('-'*20)
+    else:
+        break
+    c += 1
+print()
+print(f'Errou! Eu escolhi o numero {nPC} e a some deu {soma} \nVocê venceu {c} vez(es)!')
+print()
