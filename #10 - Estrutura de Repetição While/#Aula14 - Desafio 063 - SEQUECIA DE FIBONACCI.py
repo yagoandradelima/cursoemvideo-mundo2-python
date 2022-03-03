4b3a8c41b9f049e908afbd5484598de67e655568
Yago Andrade <yagoandradelima@outlook.com>
2022-03-02 22:03:41 -0300

    Update #Aula14 - Desafio 063 - SEQUECIA DE FIBONACCI.py

--- #10 - Estrutura de Repetição While/#Aula14 - Desafio 063 - SEQUECIA DE FIBONACCI.py
+++ #10 - Estrutura de Repetição While/#Aula14 - Desafio 063 - SEQUECIA DE FIBONACCI.py
@@ -1,5 +1,10 @@
 #Aula14 - Desafio 063
 
+#Não está correto, ele só consegue pegar uma parte do código
+#Minha estratégia não funciona pra fibonacci, vou tentar fazer sozinho depois
+
+#Esse código não funciona e eu vou corrigir ele
+
 x = 0
 y = 1
 
@@ -9,16 +14,19 @@ print('-' * 25)
 termos = int(input('Quantos termos você quer mostrar? '))
 
 while termos != 0:
-    for i in range(0, termos):
-        if termos == 1:
-            print(x)
-            termos = int(input('Quantos termos você quer mostrar? '))
-        elif termos == 2:
-            print(y)
-            termos = int(input('Quantos termos você quer mostrar? '))
-        elif termos > 2:
-            x += y
-            y += x
-            
+    z = x + y
+    if termos == 1:
+        print(x)
+        termos = int(input('Quantos termos a mais você quer mostrar? '))
+    if termos == 2:
+        print(x)
+        print(y)
+        termos = int(input('Quantos termos a mais você quer mostrar? '))
+    if termos > 2:
+        print(z)
+        x = y
+        y = z           
+    termos = int(input('Quantos termos a mais você quer mostrar? '))
+print('fim do programa')
             
 
